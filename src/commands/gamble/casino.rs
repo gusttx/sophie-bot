f8c6bd1239558db43716d768ba06945689548a22
Gustavo Bispo <36239842+gusttx@users.noreply.github.com>
2025-04-19 10:01:58 -0300

    Require subcommands for casino command

--- src/commands/gamble/casino.rs
+++ src/commands/gamble/casino.rs
@@ -4,7 +4,7 @@ use super::info::info;
 use crate::types::{Context, UnitResult};
 use poise::command;
 
-#[command(slash_command, subcommands("blackjack", "info"))]
+#[command(slash_command, subcommands("blackjack", "info"), subcommand_required)]
 pub async fn casino(_: Context<'_>) -> UnitResult {
     Ok(())
 }
